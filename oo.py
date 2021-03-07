7aaea33f707e56873fa63930a8fab6f32610b608
Missy Davies <ms.melissadavies@gmail.com>
2021-03-07 19:10:46 +0000

    Added three classes for Student, Question, Exam

--- oo.py
+++ oo.py
@@ -1,1 +1,18 @@
-# Create your classes here
+class Student:
+    def __init__(self, firstname, lastname, address):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.address = address
+
+
+class Question:
+    def __init__(self, question, correct_answer):
+        self.question = question
+        self.correct_answer = correct_answer
+
+
+class Exam:
+
+    def __init__(self, name):
+        self.questions = []
+        self.name = name 
