8ecaa59b7753cbff8f53812dec431717c8764624
Missy Davies <ms.melissadavies@gmail.com>
2021-03-07 19:42:05 +0000

    Added functions to Exam class to add a question and to Question class to ask and evaluate the question

--- oo.py
+++ oo.py
@@ -1,18 +1,39 @@
-class Student:
+class Student(object):
+    """Student information"""
+
     def __init__(self, firstname, lastname, address):
         self.firstname = firstname
         self.lastname = lastname
         self.address = address
 
 
-class Question:
+class Question(object):
+    """A question in an exam"""
+
     def __init__(self, question, correct_answer):
         self.question = question
         self.correct_answer = correct_answer
 
 
-class Exam:
+    def ask_and_evaluate(self):
+        """Print the question to the console and prompt the user for an answer"""
+
+        print(self.question)
+        guess = input("> ")
+
+        return guess == self.correct_answer
+    
+
+class Exam(object):
+    """An exam is a test.
+
+    Exams have zero or more questions"""
 
     def __init__(self, name):
         self.questions = []
         self.name = name 
+
+    def add_question(self, question):
+        """Add a question to the exam"""
+
+        self.questions.append(question)
